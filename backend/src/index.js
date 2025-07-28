63c54f82d83b1a7b31217bc939fdb65f364c305f
SEEDART007 <chakrabortysiddhartha58@gmail.com>
2025-07-29 02:24:29 +0530

    updated signup page

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const app = express();
+
 
 const authRouter = require('./routes/auth.route');
 const messageRouter = require('./routes/message.route')
@@ -9,7 +11,11 @@ const dbConnect = require('./lib/db');
 
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+ 
 app.use("/api/auth/",authRouter)
 app.use("/api/message",messageRouter)
 
